9e9008e09c2b29743669706406e1e858c7d7edde
jlyon87 <jml6487@gmail.com>
2017-10-08 20:07:21 -0400

    sso auth in progress

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,6 +1,58 @@
 var App = (function App() {
 
-	
+	var state = new Date() + "-downwardfacingdog";
+
+	var params = [
+		{
+			key: "response_type",
+			value: "code"
+		},
+		{
+			key: "redirect_uri",
+			value: esi.callbackUrl
+		},
+		{
+			key: "client_id",
+			value: esi.clientId
+		},
+		{
+			key: "scope",
+			value: esi.scopes
+		},
+		{
+			key: "state",
+			value: state
+		}
+	];
+
+	var buildAuthUri = function() {
+		var queryParams = params.reduce(function(acc, val) {
+			return acc.concat(val.key + "=" + val.value);
+		}, []);
+		console.log(queryParams.join("&"));
+		return "https://login.eveonline.com/oauth/authorize?" + queryParams.join("&");
+	};
+
+	var authResponseHandler = function(res) {
+		console.log(response);
+	};
+
+	var authorize = function() {
+		var req = new XMLHttpRequest();
+		req.addEventListener("load", authResponseHandler);
+		req.open("GET", buildAuthUri);
+		req.send();
+	};
+
+	var init = function() {
+		var url = buildAuthUri();
+		console.log(url);
+	};
+	init();
+
+	App.controller = {
+		authorize: authorize
+	};
 
 	return App;
 } (App || {}));
--- server/esiApi.js
+++ server/esiApi.js
@@ -1,14 +1,42 @@
+const axios = require("axios");
 const apiConfig = require("./apiConfig.js");
 
-const esiConfig;
+// apiConfig.getEsiScopes()
+// 	.then((scopes) => {
+// 		apiConfig.esi.scopes = scopes;
+// 		console.log(apiConfig.esi);
+// 	});
+
+const authorize = (esiConfig) => {
+	console.log(esiConfig.scopes.join(" "));
+	axios.get("https://login.eveonline.com/oauth/authorize", {
+		params: {
+			response_type: "code",
+			redirect_uri: esiConfig.scopes,
+			client_id: esiConfig.clientId,
+			scope: esiConfig.scopes.join(" "),
+			state: "downwardfacingdog"
+		}
+	})
+};
+
+const handleBearer = (response) => {
+	console.log(response);
+};
+
+const handleAuthErr = (err) => {
+	throw err;
+};
 
 const init = () => {
-	esiConfig = apiConfig.esi()
-		.then((esiConfig) => {
-			return esiConfig;
+	apiConfig.getEsiScopes()
+		.then((scopes) => {
+			apiConfig.esi.scopes = scopes;
+			return apiConfig.esi;
 		})
-		.then((esiConfig) => {
-			console.log("esiConfig", esiConfig);
-		});
+		.then(authorize)
+		.then(handleBearer)
+		.catch(handleAuthErr);
 };
+
 init();
